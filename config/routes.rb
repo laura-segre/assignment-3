1dce1ab8c628e8e8040d5fb4899becc6cab4f6ae
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-02-21 19:23:38 +0000

    created routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/places",  :controller => "places", :action => "index" )
+  get("/entries",  :controller => "entries", :action => "index" )
 end
